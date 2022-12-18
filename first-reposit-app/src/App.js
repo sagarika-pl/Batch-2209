53962e71498c90d10f8fbe0a95f6fd17c859c7bf
sagarika <sagarika-pl@github.com>
2022-12-18 15:15:36 +0530

    added second local heading

--- first-reposit-app/src/App.js
+++ first-reposit-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is added from my repository</h1>
+      <h1>This is second heading from local repository</h1>
     </div>
   );
 }
